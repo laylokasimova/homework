e29b1c5eecf6c13160235fa305f627a5b76afb2c
diyorbekjoraqulov8 <diyorbekjoraqulov8@gmail.com>
2023-01-12 18:20:14 +0300

    first commit

---
+++ 4.7-JavaScript-6/main.js
@@ -0,0 +1,26 @@
+let numArr = [1, 2, 4, 3, 5, 6, 8, 10, 13, 12, 20, ]
+let answer = 0
+
+let numArr1 = [1, 2, 4, 3, 5, 6, 8, 10, 13, 12, 20, ]
+let answer1 = 0
+
+for (let i = 0; i < numArr.length; i++) {
+    answer += numArr[i] % 2 == 0 ? 1 : 0;
+}
+
+
+for (let i = 0; i < numArr1.length; i++) {
+    answer1 += numArr1[i] % 2 != 0 ? 1 : 0;
+}
+
+console.log(`Jami: ${answer} juft soni mavjud`);
+console.log(`Jami: ${answer1} toq soni mavjud`);
+
+
+for (let i = 0; i < numArr.length; i++) {
+    if (numArr[i] % 2 == 0) {
+        console.log(numArr[i] + ' juft');
+    } else {
+        console.log(numArr[i] + ' toq');
+    }
+}
